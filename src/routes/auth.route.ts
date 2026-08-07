896973b6e935c7c2b6507be516aa1c4c3f19c49e
AbdulrahmanAbdulhakeem <abdulrahmanabdulhakeem952@gmail.com>
2026-08-07 22:11:26 +0100

    seperated the routes in auth.route to avoid conflicts

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,16 +1,30 @@
 import { Router } from "express";
-import { adminCreateCompany, adminUpdateCompany, deleteCompanyProfile, getAdminStats, getAllCompaniesAdmin, getCompanyProfile, updateCompanyProfile } from "../controllers/admin.controller.js";
+import {
+  adminCreateCompany,
+  adminUpdateCompany,
+  deleteCompanyProfile,
+  getAdminStats,
+  getAllCompaniesAdmin,
+  getCompanyProfile,
+  updateCompanyProfile,
+} from "../controllers/admin.controller.js";
 import { requireAuth, requireRole } from "../middleware/auth.middleware.js";
 
 const router = Router();
-router.use(requireAuth,requireRole("ADMIN"))
+router.use(requireAuth, requireRole("ADMIN"));
 
 // Protect this route: must be logged in, and role must match 'ADMIN'
 //Testing using the adminCreateCompany,the main utility is the adminCreateCompanyFromRegistry
 router.post("/companies/onboard", adminCreateCompany);
 router.get("/companies", getAllCompaniesAdmin);
-router.route('/companies/:userId').get(getCompanyProfile).put(updateCompanyProfile).delete(deleteCompanyProfile)
 router.get("/stats", getAdminStats);
+
+
+router.get("/companies/:userId", getCompanyProfile);
+router.put("/companies/:userId", updateCompanyProfile);
+router.delete("/companies/:userId", deleteCompanyProfile);
+
+
 router.put("/companies/:id", adminUpdateCompany);
 
 export default router;
